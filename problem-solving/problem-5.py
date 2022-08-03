bbba0bf13c9a1516d3d10a5512c44a58b5e4eac5
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-08-03 22:21:30 +0600

    problem-5

---
+++ problem-solving/problem-5.py
@@ -0,0 +1,12 @@
+# Swap two variables.
+
+# To swap two variables: the value of the first variable will become the value of the second variable. On the other hand, the value of the second variable will become the value of the first variable.
+
+
+a = 5
+b = 9
+print('a , b ', a, b)
+tem = a
+a = b
+b = tem
+print('a ,b ', a, b)
