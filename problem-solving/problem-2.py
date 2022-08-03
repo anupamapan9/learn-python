53c750768d333852d4aa4f86fa9e739f2fc9ff0d
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-08-03 20:19:27 +0600

    problem solve

---
+++ problem-solving/problem-2.py
@@ -0,0 +1,5 @@
+# Take two numbers from the users. Calculate the result of second number power of the first number.-----------------------------
+number1 = int(input("Enter your integer number: "))
+number2 = int(input("Enter your float number: "))
+
+print(number1**number2)
