ce920842ff658a302f26634679a35f334dcb8268
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-08-03 20:13:24 +0600

    problem-1 solved

---
+++ problem-solving/problem-sloving-1.py
@@ -0,0 +1,6 @@
+# Take two inputs from the user. One will be an integer. The other will be a float number. Then multiply them to display the output.
+
+number1 = int(input("Enter your integer number: "))
+number2 = float(input("Enter your float number: "))
+
+print(number1*number2)
