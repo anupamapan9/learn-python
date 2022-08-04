d50900767d321c4270bdda6af54103bb0903ac4b
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-08-04 09:38:14 +0600

    day-2 finished

--- day-2/dictionaries.py
+++ day-2/dictionaries.py
@@ -1,1 +1,11 @@
 # dictionaries is like object is Java scripts---------
+
+
+# key cannot be repitrs
+my_details = {
+    'name': 'Anupam',
+    'nationality': 'Bangladesh',
+    'Age': 23,
+}
+print(my_details['name'])
+print(my_details['nationality'])
